9343f0917e69725b47f863eb8dd6f63d95bee1a9
GeekchanskiY <astrovskiy.04@gmail.com>
2022-12-19 04:30:24 +0300

    detail page ready

--- src/components/users/user_detail.jsx
+++ src/components/users/user_detail.jsx
@@ -3,98 +3,81 @@ import { useSelector, useDispatch } from 'react-redux';
 import { set }  from "../../features/userSlice";
 import { refresh , auth }  from "../../features/JWTSlice";
 import { useState, useEffect } from 'react';
-import refresh_request from "../../utils/refresh_token";
-import check_token from "../../utils/check_token";
+import refresh_request from "../../utils/requests/user/refresh_token";
+import check_token from "../../utils/requests/user/check_token";
 import { useNavigate } from "react-router-dom";
+import user_data_request from "../../utils/requests/user/user_data";
+import logout_action from "../../utils/actions/user/logout";
+import { server_url } from "../../app/constants";
+import logo from "../../img/SafeCoin.png";
 
-export function User_detail_info(){
+export default function UserDetail(){
     const jwt_state = useSelector((state) => state.jwt)
     const user_state = useSelector((state) => state.User)
+    const [pageinit, setPageInit] = useState(true)
     const [user, setUser] = useState({
         'email': ''
     })
+    const [about_me, setAboutMe] = useState("")
+    const [country, setCountry] = useState("")
+
     const navigate = useNavigate()
     
     const dispatch = useDispatch()
 
-    useEffect(() => {
-        const f = async () => {
-            if (jwt_state.authentificated == false){
-                navigate('/login')
-            }
-            if (check_token(jwt_state.expires_in) == true){
-                console.log("refresh_token")
-                let new_token = refresh_request(jwt_state.refresh_token)
-                dispatch(refresh(new_token))
-            }
-            const url = "http://127.0.0.1:8000/api/users/me/"
-            const requestOptions = {
-                method: 'GET',
-                headers: { 
-                    'Content-Type': 'application/json',
-                    'Authorization': 'Bearer '+jwt_state.token
-            },
-                
-            };
-        
-        
-            const response = await fetch(url, requestOptions)
-            const data = await response.json()
-            if (response.ok){
-                setUser(data)
-            } else {
-                console.log("Blyat")
-                
-            }
-        }
-        f();
-    })
+    const set_user_data = async () => {
+        let user_data = await user_data_request()
+        setUser(user_data)
+        setAboutMe(user_data.about_me)
+        setCountry(user_data.country)
+    }
 
-    const get_data = async () => {
+    useEffect(() => {
         if (jwt_state.authentificated == false){
             navigate('/login')
         }
-        if (check_token(jwt_state.expires_in) == true){
-            console.log(jwt_state)
-            let new_token = await refresh_request(jwt_state.refresh_token)
-            await dispatch(refresh(new_token))
-        }
-        const url = "http://127.0.0.1:8000/api/users/me/"
-        const requestOptions = {
-            method: 'GET',
-            headers: { 
-                'Content-Type': 'application/json',
-                'Authorization': 'Bearer '+jwt_state.token
-        },
-            
-        };
-    
-        const response = await fetch(url, requestOptions)
-        const data = await response.json()
-        if (response.ok){
-            setUser(data)
-        } else {
-            console.log("Blyat")
-            
+        
+        if (pageinit){
+            set_user_data()
+            setPageInit(false)
         }
         
+        
+        
+            
+        
+    })
+
+    if (user.email === ''){
+        return <div>please wait data loads</div>
     }
 
     return (
-        <div>
-            <span>{user.email}</span>
-            <button onClick={async (e) => {get_data()}}>refresh</button>
+        <div className="user_detail">
+            <div className="welcomeback">
+                <img className="avatarimg" src={server_url + user.avatar} alt="avatar" />
+                <h3 className="welcomeback_h3">Welcome back, <span className="green">{user.username}</span></h3>
+                <img className="logoimg" src={logo} alt="logo" />
+            </div>
+            <hr />
+            <div className="userinfoblock">
+                <span className="email">Your email is: {user.email}</span> <br />
+                <span className="blocklabel">Change Password:</span> <br />
+                <input type="password" placeholder="Old password" name="old_password" id="old_password"/> <br /> <br />
+                <input type="password" placeholder="New password" name="new_password" id="new_password"/><br />
+                <input type="password" placeholder="Confirm password" name="confirm_password" id="confirm_password"/> <br />
+                <input type="button" value="confirm" />
+                <h3>Personal info</h3>
+                <div className="personal">
+                <span>Country:</span>
+                <input type="text" name="about_me" id="about_me_form" value={country} onChange={(e) => setCountry(e.target.value)}/> <div></div><input type="button" value="Update" /> 
+                <span>About me:</span><input type="text" name="about_me" id="about_me_form" value={about_me} onChange={(e) => setAboutMe(e.target.value)}/> <div></div><input type="button" value="Update" /> 
+                </div>
+                
+            </div>
+            <br />
+            <button onClick={async (e) => {set_user_data()}}>refresh</button>
+            <button onClick={(e) => {logout_action()}}>logout</button>
         </div>
     )
-}
-
-export default class UserDetail extends React.Component{
-    
-    constructor(props){
-        super(props)
-    }
-
-    render(){
-        return <div><User_detail_info/></div>
-    }
 }
