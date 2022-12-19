22af624d7ffa8cf2e132108c3e91721dd2e79f04
GeekchanskiY <astrovskiy.04@gmail.com>
2022-12-19 04:28:25 +0300

    moved/deleted source

--- src/utils/check_token.js
+++
@@ -1,9 +0,0 @@
-export default function check_token(expires_in){
-    let now = new Date()
-    let expires = new Date(expires_in)
-    if (now.getTime() > expires.getTime()){
-        return true
-    } else {
-        return false
-    }
-}
