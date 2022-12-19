f25e6958f7a8b570c8ef043ada885ff10902bfcb
GeekchanskiY <astrovskiy.04@gmail.com>
2022-12-19 11:58:05 +0300

    detail info request created

--- src/utils/requests/user/change_about_me.js
+++ src/utils/requests/user/change_about_me.js
@@ -1,7 +1,9 @@
 import { server_url } from "../../../app/constants";
 import store from "../../../app/store";
+import check_token from "./check_token";
 
 export default async function change_about_me_request(about_me){
+    check_token()
     const jwt_state = store.getState().jwt
     const url = server_url + "/api/users/change_about_me/"
     const requestOptions = {
