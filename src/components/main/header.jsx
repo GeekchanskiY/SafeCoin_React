47dd22f0f49068a158b5fcc21050335a62073c31
GeekchanskiY <astrovskiy.04@gmail.com>
2022-12-19 04:29:53 +0300

    auth ready

--- src/components/main/header.jsx
+++ src/components/main/header.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import logo from "../../img/SafeCoin 2.png";
+import logo from "../../img/SafeCoin.png";
 import search from '../../img/search.png'
 import trending from '../../img/trending.png';
 import like from '../../img/like.png';
@@ -7,14 +7,18 @@ import user from '../../img/user.png';
 
 import { useSelector, useDispatch } from 'react-redux';
 import { useEffect } from "react";
+import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 
 export default function Header () {
     const user_state = useSelector((state) => state.user)
+    const navigate = useNavigate()
     return <header>
-        <img src={logo} alt="" id="headerlogo" />
-        <a href=""><img src={user} alt="" /></a>
         
+        <img src={logo} alt="" id="headerlogo" onClick={() => {navigate('/')}}/>
+        {user_state.username === null ? <Link to={'/login'}><img src={user} alt="" /></Link> : <Link to={'/user'}><img src={user_state.userimg} alt="" /></Link>}
+
         <a href=""><img src={like} alt="" /></a>
 
         <a href=""><img src={trending} alt="" /></a>
