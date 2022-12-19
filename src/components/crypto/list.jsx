eee4f881bab4f7bc39c1ddb508ba5b55e2b36e23
GeekchanskiY <astrovskiy.04@gmail.com>
2022-12-19 11:58:45 +0300

    re-init

--- src/components/crypto/list.jsx
+++ src/components/crypto/list.jsx
@@ -11,54 +11,7 @@ class CryptoDetail extends React.Component{
 }
 
 
-export default class CryptoList extends React.Component{
-    requestOptions = {
-        method: 'GET',
-    };
-    constructor(props){
-        super(props);
-        this.state = {
-            isLoaded: false,
-            data: null
-        }
-        fetch("http://127.0.0.1:8000/api/cryptos/", this.requestOptions)
-        .then(res => res.json())
-        .then(
-            (result) => {
-            this.setState({
-                isLoaded: true,
-                data: result
-            })
-        },
-            (error) => {
-            this.setState({
-                isLoaded: false,
-                data: null
-            })
-        }
-        )
-    }
-  
+export default function CryptoList (){
     
-    
-    render(){
-        if (this.state.data !== null){
-            return <div className="a">
-            
-            <div className="items">
-                {this.state.data.results.map((element, index) => {
-                    return <CryptoDetail key={"cr"+index} data={element}></CryptoDetail>
-                })}
-            </div>
-            <div className="paginator">
-                <div className="page current">
-                    {Math.floor(this.state.data.count%20)}
-                </div>
-            </div>
-        </div>
-        } else {
-            return <h1>Error occured</h1>
-        }
-        
-    }
+    return <div></div>
 }
