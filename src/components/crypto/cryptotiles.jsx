e618cad71a273d5da1024093573e5e3528ce9230
GeekchanskiY <astrovskiy.04@gmail.com>
2022-12-21 04:36:26 +0300

    init

--- src/components/crypto/cryptotiles.jsx
+++ src/components/crypto/cryptotiles.jsx
@@ -1,80 +1,19 @@
-
-import React from "react";
 import { Link } from "react-router-dom";
+import { useState } from "react";
 
-export class Tile extends React.Component{
-    constructor(props){
-        super(props);
-        this.state = {
-            detail: false
-        }
-    }
+export default function CryptoTile(props){
+    const data = props.data;
+    console.log(data)
+    const [hidden, setHidder] = useState(false)
+    if (!hidden){
+        return <div className="cryptotile">
+            <img src={data.image} alt={data.name} />
+            <span>{data.name}</span>
+        </div>
+    } else {
+        return <div className="cryptorile">
 
-    setvisible(){
-        this.setState({
-            detail: !this.state.detail
-        })
-        // if to remove deadlock
-        if (!this.state.detail){
-            setTimeout(() => {
-                this.setvisible()
-            }, 3000);
-        }
-        
+        </div>
     }
-
-    render(){
-        if (!this.state.detail){
-            return <div className="cryptotile">
-                <img src={this.props.data.image} alt="" onMouseEnter={(e) => {this.setvisible()}}/>
-                <br/>  
-                <span onClick={(e) => {this.setvisible()}}>{this.props.data.name}</span>
-            </div>
-        } else {
-            let link = '/crypto/'+ this.props.data.name;
-            return <div className="cryptotile">
-                <span onClick={(e) => {this.setvisible()}}>{this.props.data.name} </span><small>{this.props.data.code}</small><br />
-                <Link to={link}>aaa</Link>
-            </div>
-        }
-    }
-}
-
-export default class CryptoTiles extends React.Component{
-    constructor(props){
-        super(props)
-        this.state = {
-            isLoaded: false,
-            data: null
-        }
-        fetch("http://127.0.0.1:8000/api/cryptos/", this.requestOptions)
-        .then(res => res.json())
-        .then(
-            (result) => {
-            this.setState({
-                isLoaded: true,
-                data: result
-            })
-        },
-            (error) => {
-            this.setState({
-                isLoaded: false,
-                data: null
-            })
-        }
-        )
-    }
-    render(){
-        if (this.state.isLoaded){
-            let data = this.state.data.results.slice(0, 4)
-            return <div><h1>Newest Cryptos Now:</h1>
-            <div className="cryptotilesblock">
-            {data.map((el, index) => {return <Tile data={el} key={'CryptoTile'+index} ></Tile>})}
-            </div>
-            
-            </div>
-        } else {
-            return <h3>data cant be loaded</h3>
-        }
-    }
+    
 }
