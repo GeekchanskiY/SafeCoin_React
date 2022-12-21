5eafafad3c481efc77e4d91ab47d8dc7776fdb82
GeekchanskiY <astrovskiy.04@gmail.com>
2022-12-21 04:38:25 +0300

    logic changed

--- src/utils/requests/user/check_token.js
+++ src/utils/requests/user/check_token.js
@@ -2,13 +2,13 @@ import { useSelector } from "react-redux"
 import store from "../../../app/store"
 import refresh_token from "./refresh_token"
 
-export default function check_token(){
+export default async function check_token(){
     const jwt_state = store.getState().jwt
     let now = new Date()
     
     let expires = new Date(jwt_state.expires_in)
     if (now.getTime() > expires.getTime()){
-        refresh_token()
+        await refresh_token()
         return true
     } else {
         return false
