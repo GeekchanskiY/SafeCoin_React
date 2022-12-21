94b7b2b372e8df2ba7a34efc8d5b5469e3961a0a
GeekchanskiY <astrovskiy.04@gmail.com>
2022-12-21 04:36:20 +0300

    localstorage middleware added

--- src/app/store.jsx
+++ src/app/store.jsx
@@ -1,10 +1,22 @@
-import { configureStore } from '@reduxjs/toolkit'
+import { compose, configureStore } from '@reduxjs/toolkit'
 import JWTReducer from '../features/JWTSlice'
 import userReducer from '../features/userSlice'
+import persistState from 'redux-localstorage'
+
+const authMiddleware = (store) => (next) => (action) => {
+    const result = next(action);
+    if (action.type == "JWT/auth"){
+        localStorage.setItem('auth', JSON.stringify(action.payload))
+        console.log('YAY')
+    }
+    
+    return result;
+  };
 
 export default configureStore({
     reducer: {
         jwt: JWTReducer,
         user: userReducer
     },
+    middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(authMiddleware)
   })
