ffa580b8c41df387e919d9121690bc58737a4d61
GeekchanskiY <astrovskiy.04@gmail.com>
2022-12-23 01:46:38 +0300

    list ready

--- src/components/crypto/list.jsx
+++ src/components/crypto/list.jsx
@@ -4,10 +4,56 @@ import { useState, useEffect } from "react";
 
 import crypto_list_request from "../../utils/requests/crypto/get_cryptos";
 
+import { CryptoCanvas } from "./cryptodetail";
+import { get_crypto_prices } from "../../utils/requests/crypto/get_cryptos";
+import { Link } from "react-router-dom";
+
 // Fun fact -- в девелопменте реакт рендерит все компонеты дважды
 
 
+export function CryptoListItem (props){
+    const [pageinit, setPageInit] = useState(true)
+    const [haveCanvas, setHaveCanvas] = useState(false)
+    const [pricePoints, setPricePoints] = useState([])
+    const get_detail = async () => {
+        const pdata = await get_crypto_prices(props.data.name, 1)
+        
+        if (pdata != null){
+            setPricePoints(pdata)
+            
+            setHaveCanvas(true)    
+            
+        }
+         
+        
+    }
+    useEffect(()=>{
+        if (pageinit){
+            get_detail()
+            console.log(pricePoints)
+            setPageInit(false)
+            
+        }
+    })
 
+    return <div className="CryptoListItem">
+        <div className="imageholder">
+            <img src={props.data.image} alt="" />
+        </div>
+        <div className="nameholder">
+            <h3><Link to={"/crypto/"+props.data.name+"/"}>{props.data.name}</Link></h3>
+        </div>
+        <div className="dataholder">
+            <span>Marker cap $:</span><span>{props.data.market_cap}</span>
+            <span>24H volume $:</span><span>{props.data.volume}</span>
+            <span>24h transactions:</span><span>{props.data.transactions_count}</span>
+        </div>
+        <div className="canvasholder">
+            {haveCanvas == true ? <CryptoCanvas points={pricePoints} > </CryptoCanvas> : <span>Data unavailable</span>}
+        </div>
+        
+    </div>
+}
 
 export default function CryptoList (){
     const [cryptos, setCryptos] = useState([])
@@ -46,25 +92,32 @@ export default function CryptoList (){
     
     
     return <div>
-        <div className="listtiles">
-        {cryptos.map((crypto, index) => {
-            return <CryptoTile data={crypto} key={"crypto"+index}></CryptoTile>
-        })}
+        <div className="cryptosearch">
+            <div className="cryptofilters">
+
+            </div>
+            <div className="cryptolist">
+            {cryptos.map((crypto, index) => {
+                return <CryptoListItem data={crypto} key={"crypto"+index}></CryptoListItem>
+            })}
+            <div className="pagination">
+                {have_next ?
+                    <button onClick={next_page}>next</button>
+                    :
+                    null
+                }
+                <span className="current_page">{page}</span>
+                {have_prev ?
+                    <button onClick={prev_page}>prev</button>
+                    :
+                    null
+                }
+            </div>
+            </div>
+        
+            
         </div>
-    
-    <div className="pagination">
-            {have_next ?
-                <button onClick={next_page}>next</button>
-                :
-                null
-            }
-            <span className="current_page">{page}</span>
-            {have_prev ?
-                <button onClick={prev_page}>prev</button>
-                :
-                null
-            }
-        </div>
+        
     </div>
     
     
